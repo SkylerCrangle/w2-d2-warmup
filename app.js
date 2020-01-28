df523b203bc4f495cc5d7161f0c4401aae05a257
SkylerCrangle <smcrangle@gmail.com>
2020-01-28 13:59:58 -0700

    blep

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let glassBottle = {
+  theColor: "green",
+  theWeight: "heavy",
+  transparent: true,
+  lid: "attatched lid",
+  fill: ["water", "tea", "juice", "beer", "otherliquids"],
+  alive: false,
+}
+
